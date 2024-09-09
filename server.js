c08777e815570c09906d666781522b5917bd1a86
Muhammad Fadhlan <125030998+ArraySonic@users.noreply.github.com>
2024-09-09 20:17:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Set direktori root untuk file html
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+app.set('view engine', 'ejs'); // Ganti 'html' dengan 'ejs'
 
 // Router untuk menghapus .html dari file portofolio.html
 app.get('/portofolio', (req, res) => {
@@ -24,5 +24,5 @@ const port = process.env.PORT || 3000;
 
 // Jalankan server
 app.listen(port, () => {
-  console.log(`Server is running at http://forlann.live:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
